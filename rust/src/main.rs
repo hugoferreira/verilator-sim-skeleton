8db030c84207e2c4c808a8ad56066abe4300a1da
Hugo Ferreira <bytter@gmail.com>
2021-02-21 01:14:28 +0000

    Push pixels to frame buffer every cycle.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -21,7 +21,6 @@ pub struct Top {
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-
     let mut window = Window::new(
         "Screen (ESC to Exit)",
         WIDTH,
@@ -33,45 +32,48 @@ fn main() {
     });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    window.limit_update_rate(Some(Duration::from_micros(16600)));
 
     let mut tb = Top::default();
     tb.eval();
     tb.eval();
 
-    tb.open_trace("counter.vcd", 99).unwrap();
+    // tb.open_trace("counter.vcd", 99).unwrap();
 
     let mut clocks: u64 = 0;
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = (u32::from(tb.count_o()) << 16) + (u32::from(tb.count_o()) << 8) + u32::from(tb.count_o()); // write something more funny here!
-        }
-
-        if clocks == 0 {
-            tb.reset_toggle();
-        } else if clocks == 2 {
-            tb.reset_toggle();
-        }
-
+    tb.reset_toggle();
+    while clocks <= 10 {
         tb.clock_toggle();
         tb.eval();
-        tb.trace_at(Duration::from_nanos(20 * clocks));
-
-        tb.clock_toggle();
-        tb.eval();
-        tb.trace_at(Duration::from_nanos(20 * clocks + 10));
-
-        println!("{}: count_o = {}", clocks, tb.count_o());
-
+        // tb.trace_at(Duration::from_nanos(10 * clocks));
         clocks += 1;
-
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
     }
 
-    tb.trace_at(Duration::from_nanos(20 * clocks));
+    tb.reset_toggle();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {            
+            tb.clock_toggle();
+            tb.eval();
+            // tb.trace_at(Duration::from_nanos(20 * clocks));
+
+            let color = (u32::from(tb.count_o()) << 16) + (u32::from(tb.count_o()) << 8) + u32::from(tb.count_o());
+            *i = color;
+
+            tb.clock_toggle();
+            tb.eval();
+            // tb.trace_at(Duration::from_nanos(20 * clocks + 10));
+
+            clocks += 1;
+        }
+
+        println!("Frame {}", clocks);
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
+
+    // tb.trace_at(Duration::from_nanos(20 * clocks));
     tb.finish();
 }
