339dc31fb0575f32862a3e9970eac0aff0bc2f82
Hugo Sereno Ferreira <bytter@gmail.com>
2021-02-21 13:19:25 +0000

    Cleanup and optimisations.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -19,6 +19,13 @@ pub struct Top {
     pub count_o: [bool; 8],
 }
 
+fn tickdesign(tb: &mut Top, clocks: &mut u64) {
+    // tb.trace_at(Duration::from_nanos(10 * clocks));
+    tb.clock_toggle();
+    tb.eval();
+    *clocks += 1;
+}
+
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut window = Window::new(
@@ -26,10 +33,7 @@ fn main() {
         WIDTH,
         HEIGHT,
         WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
+    ).unwrap_or_else(|e| { panic!("{}", e); });
 
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(Duration::from_micros(16600)));
@@ -43,35 +47,25 @@ fn main() {
     let mut clocks: u64 = 0;
 
     tb.reset_toggle();
-    while clocks <= 10 {
-        tb.clock_toggle();
-        tb.eval();
-        // tb.trace_at(Duration::from_nanos(10 * clocks));
-        clocks += 1;
-    }
-
+    while clocks <= 10 { tickdesign(&mut tb, &mut clocks); }
     tb.reset_toggle();
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {            
-            tb.clock_toggle();
-            tb.eval();
-            // tb.trace_at(Duration::from_nanos(20 * clocks));
+        for i in buffer.iter_mut() {
+            tickdesign(&mut tb, &mut clocks);
 
             let color = (u32::from(tb.count_o()) << 16) + (u32::from(tb.count_o()) << 8) + u32::from(tb.count_o());
             *i = color;
 
-            tb.clock_toggle();
-            tb.eval();
-            // tb.trace_at(Duration::from_nanos(20 * clocks + 10));
-
-            clocks += 1;
+            tickdesign(&mut tb, &mut clocks);
         }
 
         println!("Frame {}", clocks);
 
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap_or_else(|e| { 
+            tb.finish();
+            panic!("{}", e); 
+        });
     }
 
     // tb.trace_at(Duration::from_nanos(20 * clocks));
