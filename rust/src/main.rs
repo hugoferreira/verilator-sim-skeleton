2aa09bcc2efcfcbcb3b30500004a858680d60c5d
Hugo Ferreira <bytter@gmail.com>
2021-02-21 00:47:13 +0000

    We now have a minifb in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,13 @@
 extern crate verilated;
 extern crate verilated_module;
+extern crate minifb;
 
+use minifb::{Key, Window, WindowOptions};
 use std::time::Duration;
-
 use verilated_module::module;
+
+const WIDTH: usize = 320;
+const HEIGHT: usize = 200;
 
 #[module(top)]
 pub struct Top {
@@ -12,18 +16,38 @@ pub struct Top {
     #[port(reset)]
     pub rst_i: bool,
     #[port(output)]
-    pub count_o: [bool; 4],
+    pub count_o: [bool; 8],
 }
 
 fn main() {
-    let mut tb: Top = Top::default();
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Screen (ESC to Exit)",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut tb = Top::default();
     tb.eval();
     tb.eval();
 
     tb.open_trace("counter.vcd", 99).unwrap();
 
     let mut clocks: u64 = 0;
-    while tb.count_o() < 10 {
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = (u32::from(tb.count_o()) << 16) + (u32::from(tb.count_o()) << 8) + u32::from(tb.count_o()); // write something more funny here!
+        }
+
         if clocks == 0 {
             tb.reset_toggle();
         } else if clocks == 2 {
@@ -41,8 +65,13 @@ fn main() {
         println!("{}: count_o = {}", clocks, tb.count_o());
 
         clocks += 1;
-    }
-    tb.trace_at(Duration::from_nanos(20 * clocks));
 
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
+
+    tb.trace_at(Duration::from_nanos(20 * clocks));
     tb.finish();
 }
