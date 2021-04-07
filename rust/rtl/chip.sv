451a55adab4f7fade13c2a34efc4d9967f653bed
Hugo Sereno Ferreira <bytter@gmail.com>
2021-04-07 12:10:39 +0100

    No need for clk2 in chip; Corrected Pico8 palette.

--- rust/rtl/chip.sv
+++ rust/rtl/chip.sv
@@ -1,6 +1,6 @@
-module chip(input logic clk_1, input logic clk_2, input logic reset, 
-            input logic vsync, input logic hsync, input logic [6:0] vpos, input logic  [7:0] hpos, 
-            output logic [RGB-1:0] rgb);
+module chip(input logic clk, input logic reset, 
+            input logic vsync, input logic hsync, 
+            input logic [6:0] vpos, input logic [7:0] hpos, output logic [RGB-1:0] rgb);
 
   parameter RED = 5, GREEN = 6, BLUE = 5, RGB = RED + GREEN + BLUE, FILE = "palette565.bin";
 
@@ -13,22 +13,22 @@ module chip(input logic clk_1, input logic clk_2, input logic reset,
   addressdecoder decoder(.addr, .rw, .cpu_di, .tb_do, .sp_do, .ram_do, .tb_cs, .sp_cs, .ram_cs);
 
   // 8x64kbit Async RAM
-  ram_async #(.A(12), .D(8)) ram(.clk(~clk_1), .cs(ram_cs), .rw, .addr(addr[11:0]), .di(cpu_do), .dout(ram_do));
+  ram_async #(.A(12), .D(8)) ram(.clk(~clk), .cs(ram_cs), .rw, .addr(addr[11:0]), .di(cpu_do), .dout(ram_do));
 
   // Control Unit
-  control c0(.clk(clk_1), .reset, .vsync, .addr, .data(cpu_do), .din(cpu_di), .rw);
+  control c0(.clk, .reset, .vsync, .addr, .data(cpu_do), .din(cpu_di), .rw);
 
   // Text Video Buffer  
   logic [3:0] text_color;
   logic [RGB-1:0] trgb; 
-  textbuffer tb(.clk(~clk_1), .reset, .addr(addr[9:0]), .cs(tb_cs), .rw, .di(cpu_do), .dout(tb_do), .hpos, .vpos, .vsync, .hsync, .color(text_color));
-  palette #(.RED(RED), .GREEN(GREEN), .BLUE(BLUE), .FILE(FILE)) pal_text(.clk(clk_1), .color(text_color), .rgb(trgb));
+  textbuffer tb(.clk(~clk), .reset, .addr(addr[9:0]), .cs(tb_cs), .rw, .di(cpu_do), .dout(tb_do), .hpos, .vpos, .vsync, .hsync, .color(text_color));
+  palette #(.RED(RED), .GREEN(GREEN), .BLUE(BLUE), .FILE(FILE)) pal_text(.clk, .color(text_color), .rgb(trgb));
 
   // Video Sprites  
   logic pixel;
   logic [RGB-1:0] srgb;
-  sprite s0(.clk(~clk_1), .reset, .addr(addr[3:0]), .cs(sp_cs), .rw, .di(cpu_do), .dout(sp_do), .hpos, .vpos, .hsync, .vsync, .pixel);  
-  palette #(.RED(RED), .GREEN(GREEN), .BLUE(BLUE), .FILE(FILE)) pal_sprite(.clk(clk_1), .color(pixel ? 4'h9 : 4'h0), .rgb(srgb));
+  sprite s0(.clk(~clk), .reset, .addr(addr[3:0]), .cs(sp_cs), .rw, .di(cpu_do), .dout(sp_do), .hpos, .vpos, .hsync, .vsync, .pixel);  
+  palette #(.RED(RED), .GREEN(GREEN), .BLUE(BLUE), .FILE(FILE)) pal_sprite(.clk, .color(pixel ? 4'h9 : 4'h0), .rgb(srgb));
 
   // Basic Video Signals 
   assign rgb = srgb | trgb; 
--- rust/rtl/top.sv
+++ rust/rtl/top.sv
@@ -1,4 +1,5 @@
-module top(input clk_i, input rst_i, output hsync, output vsync, output [23:0] rgb);
+module top(input logic clk_i, input logic rst_i, 
+           output logic hsync, output logic vsync, output logic [23:0] rgb);
   localparam WIDTH = 320, HEIGHT = 240;
 
   logic       clk_4;
@@ -8,5 +9,5 @@ module top(input clk_i, input rst_i, output hsync, output vsync, output [23:0] r
   /* verilator lint_off PINMISSING */
   slower_clk clk1(.cin(clk_i), .clk_div4(clk_4), .reset(rst_i));
   hvsync_generator hvsync_gen(.clk(clk_4), .reset(rst_i), .hsync, .vsync, .hpos, .vpos);
-  chip #(.RED(8), .GREEN(8), .BLUE(8), .FILE("palette888.bin")) chip(.clk_1(clk_i), .clk_2(clk_4), .reset(rst_i), .vsync, .hsync, .vpos, .hpos, .rgb);
+  chip #(.RED(8), .GREEN(8), .BLUE(8), .FILE("palette888.bin")) chip(.clk(clk_i), .reset(rst_i), .vsync, .hsync, .vpos, .hpos, .rgb);
 endmodule
